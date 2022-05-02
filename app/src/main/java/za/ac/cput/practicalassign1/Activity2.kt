0787aa2be9db4c01e5fe9be1552592aa49180c8f
Aaron <isaac@gmail.com>
2022-05-02 13:17:16 -0400

    Practical assignment 2nd commit

---
+++ app/src/main/java/za/ac/cput/practicalassign1/Activity2.kt
@@ -0,0 +1,73 @@
+package za.ac.cput.practicalassign1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import za.ac.cput.practicalassign1.ui.theme.PRACTICALASSIGN1Theme
+
+class Activity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(text = "Name: Isaac Kissimba Nyembo", fontSize = 15.sp)
+                Text(text = "Department:Application Development", fontSize = 15.sp)
+                Text(text = "Course: Information Communication and Technology", fontSize = 15.sp)
+                Text(text = "Student Number: 219383448", fontSize = 15.sp)
+                Button(onClick = {
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Module",
+                        Modifier.padding(end = 24.dp)
+                    )
+                    Text(text = "Current Modules", fontSize = 18.sp)
+                    
+                }
+            }
+            
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Button(onClick = {
+                    val navigate = Intent(this@Activity2, MainActivity::class.java)
+                    startActivity(navigate)
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Back",
+                        Modifier.padding(end = 24.dp)
+                    )
+                    Text(text = "Back", fontSize = 18.sp)
+
+                }
+
+            }
+        }
+    }
+}
+
+
+
--- app/src/main/java/za/ac/cput/practicalassign1/MainActivity.kt
+++ app/src/main/java/za/ac/cput/practicalassign1/MainActivity.kt
@@ -2,6 +2,7 @@ package za.ac.cput.practicalassign1
 //Name :Kissimba Nyembo Isaac
 //Student Number: 219383448
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,41 +10,33 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import za.ac.cput.practicalassign1.ui.theme.PRACTICALASSIGN1Theme
 import za.ac.cput.practicalassign1.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
-
-    data class Message(var msg:String)
-    @Composable
-    fun main(message: Message) {
-        Column(modifier= Modifier
-            .background(Gray)
-            .padding(all = 6.dp)){
-
-            Text(text ="${message.msg}", color=MaterialTheme.colors.surface,
-                style= MaterialTheme.typography.body1,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp), textAlign = TextAlign.Center)}
-    }
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -55,18 +48,20 @@ class MainActivity : ComponentActivity() {
                 main(h)
 
                 Column(
-                    verticalArrangement= Arrangement.SpaceEvenly,
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement= Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
+
                 ) {
+
                     val context= LocalContext.current
                     Button(onClick = {
                         visible.value=true},
                         shape= Shapes.medium,
-                        contentPadding = PaddingValues(18.dp),
+                        contentPadding = PaddingValues(10.dp),
 
                         colors = ButtonDefaults.textButtonColors(
-                            backgroundColor = Red,
+                            backgroundColor = Color.DarkGray,
                             contentColor = White
 
                         )
@@ -74,11 +69,29 @@ class MainActivity : ComponentActivity() {
                         Icon(
                             imageVector = Icons.Filled.Warning,
                             contentDescription = "Alert",
-                            Modifier.padding(end = 24.dp)
+                            Modifier.padding(end = 10.dp)
                         )
                         Text(text = "INFO",
                             style= MaterialTheme.typography.button,
                             modifier= Modifier.padding(7.dp))
+                    }
+
+                    Column(
+
+                    ){
+                        Button(onClick = { 
+                            val navigate = Intent(this@MainActivity, Activity2::class.java)
+                            startActivity(navigate)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Send,
+                                contentDescription = "Next",
+                                Modifier.padding(end = 24.dp)
+                            )
+                            Text(text = "Start Journey", fontSize = 18.sp)
+                            
+                        }
+                        
                     }
 
                 }
@@ -88,9 +101,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Message(var msg:String)
+@Composable
+fun main(message: Message) {
+    Column(modifier= Modifier
+        .background(Gray)
+        .padding(all = 6.dp)){
 
-
-
+        Text(text ="${message.msg}", color=MaterialTheme.colors.surface,
+            style= MaterialTheme.typography.body1,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp), textAlign = TextAlign.Center)}
+}
 
 @Composable
 fun AlertDialogComponent(visible:MutableState<Boolean>)
@@ -106,13 +129,15 @@ fun AlertDialogComponent(visible:MutableState<Boolean>)
             confirmButton = {
             },
             title = { Text(text = "Information")
-                    },
+            },
             text = { Text(text = "Here I will show my Good Job done in JetpackCompose so far, according to what we have learned")
-       Modifier.size(10.dp)
+                Modifier.size(10.dp)
 
             }
         )
 
     }
 
+
 }
+
